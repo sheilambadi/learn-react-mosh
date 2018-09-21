b8fc7bc78f8aeda9d96bcde600348f172b0b4714
sheilambadi <mbadiatieno@gmail.com>
2018-09-21 13:34:51 +0300

    Convert Navbar class to sfc as has no states

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand text-white" href="">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Stateless Functional Component
 
-export default NavBar;
+const Navbar = props => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand text-white" href="">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;

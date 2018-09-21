c5c30531b98bd5a1a3bb49526df298cf86a71c56
sheilambadi <mbadiatieno@gmail.com>
2018-09-21 11:01:34 +0300

    Remove unused items

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,51 +3,24 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     value: this.props.value,
-    imageUrl: "https://picsum.photos/200",
     tags: ["tag1", "tag2", "tag3"]
   };
 
-  handleIncrement = product => {
-    console.log(product);
-
+  handleIncrement = () => {
     this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
       <div>
-        {/* <img src={this.state.imageUrl} alt="No Image" /> */}
-        {this.props.children}
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 23 })}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-
-        {/* <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul> */}
-
-        {/* {this.state.tags.length === 0 && "No tags to display"}
-
-        {this.renderTags()} */}
       </div>
-    );
-  }
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>Nothing to display</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
     );
   }
 

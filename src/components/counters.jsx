8776e539ddd6893e0e99224f520e185f8927e5a9
sheilambadi <mbadiatieno@gmail.com>
2018-09-21 12:09:35 +0300

    Implement reset button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -27,9 +27,21 @@ class Counters extends Component {
     this.setState({ counters });
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
+        <button className="btn btn-primary m-2" onClick={this.handleReset}>
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}

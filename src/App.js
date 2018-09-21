a4b10d9a61e5a0e4843b21b7cb5feaede2cc2944
sheilambadi <mbadiatieno@gmail.com>
2018-09-21 13:12:44 +0300

    Place state in top most App component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,58 @@ import Counters from './components/counters'
 import NavBar from './components/navbar'
 
 class App extends Component {
+  state = {
+    counters: [{
+        id: 1,
+        value: 4
+      },
+      {
+        id: 2,
+        value: 0
+      },
+      {
+        id: 3,
+        value: 6
+      },
+      {
+        id: 4,
+        value: 0
+      }
+    ]
+  };
+
+  handleIncrement = counter => {
+    // replicate counters array
+    const counters = [...this.state.counters];
+
+    // dont directly modify state properties
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter
+    };
+    counters[index].value++;
+    this.setState({
+      counters
+    });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({
+      counters
+    });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({
+      counters
+    });
+  };
+
   render() {
     return ( <
       React.Fragment >
@@ -14,9 +66,20 @@ class App extends Component {
       <
       main className = "container" >
       <
-      Counters / >
-      <
-      /main> < /
+      Counters counters = {
+        this.state.counters
+      }
+      onDelete = {
+        this.handleDelete
+      }
+      onIncrement = {
+        this.handleIncrement
+      }
+      onReset = {
+        this.handleReset
+      }
+      / > < /
+      main > < /
       React.Fragment >
     );
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,51 +2,17 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 6 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleIncrement = counter => {
-    // replicate counters array
-    const counters = [...this.state.counters];
-
-    // dont directly modify state properties
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <div>
-        <button className="btn btn-primary m-2" onClick={this.handleReset}>
+        <button className="btn btn-primary m-2" onClick={this.props.onReset}>
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onIncrement={this.handleIncrement}
-            onDelete={this.handleDelete}
+            onIncrement={this.props.onIncrement}
+            onDelete={this.props.onDelete}
             counter={counter}
           />
         ))}

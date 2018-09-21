548ce695aa95028892d3656c0a27fb3ffb436406
sheilambadi <mbadiatieno@gmail.com>
2018-09-21 09:44:45 +0300

    Update state by handling event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,9 +7,9 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
-  handleIncrement() {
-    console.log("Increment clicked!");
-  }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
